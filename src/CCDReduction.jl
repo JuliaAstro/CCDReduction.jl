385019ddf26096749b5a8db199632df37d014109
Siddharth <siddharthlal25@gmail.com>
2020-05-23 04:34:24 +0530

    added overscan subtraction

--- src/CCDReduction.jl
+++ src/CCDReduction.jl
@@ -1,7 +1,11 @@
 module CCDReduction
 
+using Statistics
+
 export bias_subtraction,
-       bias_subtraction!
+       bias_subtraction!,
+       overscan_subtraction,
+       overscan_subtraction!
 
 include("methods.jl")
 
--- src/methods.jl
+++ src/methods.jl
@@ -38,3 +38,38 @@ julia> bias_subtraction(frame, bias)
 * [`bias_subtraction!`](@ref)
 """
 bias_subtraction(frame::AbstractArray, bias_frame::AbstractArray) = bias_subtraction!(deepcopy(frame), bias_frame)
+
+
+"""
+    overscan_subtraction(frame::AbstractArray, overscan_frame::AbstractArray; overscan_axis = 2)
+
+In place version of [`overscan_subtraction`](@ref)
+"""
+function overscan_subtraction!(frame::AbstractArray, overscan_frame::AbstractArray; overscan_axis = 2)
+    combined_overscan = median(overscan_frame, dims = overscan_axis)
+    frame .-= combined_overscan
+    return frame
+end
+
+
+"""
+    overscan_subtraction!(frame::AbstractArray, overscan_frame::AbstractArray; overscan_axis = 2)
+
+Subtract the overscan frame from image.
+
+`overscan_axis` is the dimension along which `overscan_frame` is combined.
+
+# Example
+```jldoctest
+julia> frame = [4.0 2.0 3.0 1.0 1.0];
+
+julia> overscan_subtraction(frame, frame[:, 4:5], overscan_axis = 2)
+1×5 Array{Float64,2}:
+ 3.0  1.0  2.0  0.0  0.0
+
+```
+
+# See Also
+* [`overscan_subtraction!`](@ref)
+"""
+overscan_subtraction(frame::AbstractArray, overscan_frame::AbstractArray; overscan_axis = 2) = overscan_subtraction!(deepcopy(frame), overscan_frame, overscan_axis = overscan_axis)
