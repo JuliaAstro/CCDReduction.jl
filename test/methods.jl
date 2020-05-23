81a8ee06eb8310adbe847bdfba9c5e75c591ae5c
Siddharth <siddharthlal25@gmail.com>
2020-05-23 17:45:48 +0530

    added tests for helper function

--- test/methods.jl
+++ test/methods.jl
@@ -1,3 +1,5 @@
+using CCDReduction: axes_min_length
+
 @testset "bias subtraction" begin
     # testing non-mutating version
     @test @inferred(bias_subtraction(ones(500, 500), ones(500, 500))) == zeros(500, 500)
@@ -20,4 +22,13 @@ end
     frame = ones(500, 600)
     @inferred overscan_subtraction!(frame, (:, 540:600))
     @test frame == zeros(500, 600)
+end
+
+@testset "helper" begin
+    # testing axes_min_length
+    @test axes_min_length((:, :)) == 1
+    @test axes_min_length((:, 5:10)) == 2
+    @test axes_min_length((1:2, :)) == 1
+    @test axes_min_length((1:2, 5:12)) == 1
+    @test axes_min_length((10:100, 1:5)) == 2
 end
