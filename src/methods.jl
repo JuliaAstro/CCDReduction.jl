412dc02a76747126d21ffd51fb915696746a9d6a
Siddharth <siddharthlal25@gmail.com>
2020-05-28 05:56:33 +0530

    fixed docs

--- src/methods.jl
+++ src/methods.jl
@@ -195,7 +195,8 @@ end
 
 Crops `frame` to the size specified by `shape` anchored by the frame center.
 
-This will remove rows/cols of the `frame` equally on each side. When there is an uneven difference in sizes (e.g. size 9 -> 6 can't be removed equally) the default is to increase the output size (e.g. 6 -> 7) so there is equal removal on each side. To disable this, set `force_equal=false`, which will remove the extra slice from the end of the axis.
+This will remove rows/cols of the `frame` equally on each side. When there is an uneven difference in sizes (e.g. size 9 -> 6 can't be removed equally) the default is to 
+increase the output size (e.g. 6 -> 7) so there is equal removal on each side. To disable this, set `force_equal=false`, which will remove the extra slice from the end of the axis.
 
 # Examples
 ```jldoctest
