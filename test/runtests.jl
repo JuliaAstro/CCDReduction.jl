d009f6183b39cfe8cb0fbea328c32ec22230c72a
Siddharth <siddharthlal25@gmail.com>
2020-06-04 18:38:48 +0530

    organised files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,5 @@ using Test
 using Statistics
 using FITSIO
 
-const test_file_url_M6707HH = "https://www.cfa.harvard.edu/~dfabricant/huchra/ay16/fits/M6707HH.fits"
-const test_file_path_M6707HH = joinpath(@__DIR__, "data", "MH6707HH.fits")
-if !isfile(test_file_path_M6707HH)
-	mkpath(dirname(test_file_path_M6707HH))
-	download(test_file_url_M6707HH, test_file_path_M6707HH)
-end
-const M6707HH =  FITS(test_file_path_M6707HH)
-
+include("data.jl")
 include("methods.jl")
