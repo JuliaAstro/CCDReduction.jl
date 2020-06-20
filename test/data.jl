8d2766d31285b852fbdb5333e6cb3edf50ee5e59
siddharthlal25 <siddharthlal25@gmail.com>
2020-06-21 03:15:41 +0530

    fixed filename

--- test/data.jl
+++ test/data.jl
@@ -1,6 +1,6 @@
 # This part is used for loading the data for testing purpose
 const test_file_url_M6707HH = "https://www.cfa.harvard.edu/~dfabricant/huchra/ay16/fits/M6707HH.fits"
-const test_file_path_M6707HH = joinpath(@__DIR__, "data", "MH6707HH.fits")
+const test_file_path_M6707HH = joinpath(@__DIR__, "data", "M6707HH.fits")
 if !isfile(test_file_path_M6707HH)
     mkpath(dirname(test_file_path_M6707HH))
     download(test_file_url_M6707HH, test_file_path_M6707HH)
