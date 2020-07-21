506f40588cfce7de2d58db0b21e942c92167d11e
Miles Lucas <mdlucas@hawaii.edu>
2020-07-21 09:44:12 -0500

    tidy up array interface

--- src/datatype.jl
+++ src/datatype.jl
@@ -6,15 +6,23 @@ struct CCDData{T,M<:AbstractMatrix{T}} <: AbstractCCDData{T}
     hdr::FITSHeader
 end
 
+CCDData(hdu::ImageHDU) = CCDData(getdata(hdu), read_header(hdu))
+
 # extending the AbstractMatrix interface
-Base.size(ccd::CCDData) = size(A.data)
+Base.size(ccd::CCDData) = size(ccd.data)
 Base.getindex(ccd::CCDData, inds...) = getindex(ccd.data, inds...) # default fallback for operations on Array
 Base.setindex!(ccd::CCDData, v, inds...) = setindex!(ccd.data, v, inds...) # default fallback for operations on Array
-Base.eachindex(ccd::CCDData) = eachindex(ccd.data)
-Base.iterate(ccd::CCDData) = iterate(ccd.data)
-Base.iterate(ccd::CCDData, state) = iterate(ccd.data, state)
-Base.length(ccd::CCDData) = length(ccd.data)
-Base.axes(ccd::CCDData) = axes(ccd.data)
-Base.IndexStyle(::CCDData) = IndexCartesian()
 
-CCDData(hdu::ImageHDU) = CCDData(getdata(hdu), read_header(hdu))
+# broadcast mechanics
+Base.BroadcastStyle(::Type{<:CCDData}) = Broadcast.ArrayStyle{CCDData}()
+function Base.similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{CCDData}}, ::Type{T}) where T
+    ccd = find_ccd(bc)
+    CCDData(similar(ccd.data, T, axes(bc)), ccd.hdr)
+end
+"`A = find_ccd(As)` returns the first CCDData among the arguments."
+find_ccd(bc::Base.Broadcast.Broadcasted) = find_ccd(bc.args)
+find_ccd(args::Tuple) = find_ccd(find_ccd(args[1]), Base.tail(args))
+find_ccd(x) = x
+find_ccd(::Tuple{}) = nothing
+find_ccd(a::CCDData, rest) = a
+find_ccd(::Any, rest) = find_ccd(rest)
