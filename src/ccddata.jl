38d1213ece08f4591c9778d4a48705b55a0307d8
siddharthlal25 <siddharthlal25@gmail.com>
2020-08-03 09:47:51 +0530

    modified interface functions

--- src/ccddata.jl
+++ src/ccddata.jl
@@ -6,6 +6,12 @@ Supertype for `CCDData` based on `AbstractMatrix` interface.
 """
 abstract type AbstractCCDData{T} <: AbstractMatrix{T} end
 
+# extending the AbstractMatrix interface
+Base.size(ccd::AbstractCCDData) = size(data(ccd))
+Base.getindex(ccd::AbstractCCDData, inds...) = getindex(data(ccd), inds...) # default fallback for operations on Array
+Base.setindex!(ccd::AbstractCCDData, v, inds...) = setindex!(data(ccd), v, inds...) # default fallback for operations on Array
+Base.promote_rule(::Type{AbstractCCDData{T}}, ::Type{AbstractCCDData{V}}) where {T,V} = AbstractCCDData{promote_type{T,V}}
+
 # custom data type to hold ImageHDU
 """
     CCDData(data::AbstractMatrix, hdr::FITSHeader)
@@ -39,22 +45,17 @@ Loads HDU from `hdu` index in `FITS` file at `path` as `CCDData`.
 CCDData(path::AbstractString; hdu = 1) = FITS(f -> CCDData(f[hdu]), path)
 
 CCDData{T}(data, hdr) where {T<:Number} = CCDData(T.(data), hdr)
-
-# extending the AbstractMatrix interface
-Base.size(ccd::CCDData) = size(ccd.data)
-Base.getindex(ccd::CCDData, inds...) = getindex(ccd.data, inds...) # default fallback for operations on Array
-Base.setindex!(ccd::CCDData, v, inds...) = setindex!(ccd.data, v, inds...) # default fallback for operations on Array
-Base.promote_rule(::Type{CCDData{T}}, ::Type{CCDData{V}}) where {T,V} = CCDData{promote_type{T,V}}
-Base.convert(::Type{CCDData{T}}, ccd::CCDData{V}) where {T,V} = CCDData{T}(ccd.data, ccd.hdr)
-Base.view(ccd::CCDData, inds...) = CCDData(view(ccd.data, inds...), ccd.hdr)
-Base.selectdim(ccd::CCDData, d::Integer, idxs) = CCDData(selectdim(ccd.data, d, idxs), ccd.hdr)
-Base.copy(ccd::CCDData) = CCDData(copy(ccd.data), deepcopy(ccd.hdr))
-
+data(ccd::CCDData) = ccd.data
+hdr(ccd::CCDData) = ccd.hdr
+Base.copy(ccd::CCDData) = CCDData(copy(data(ccd)), deepcopy(hdr(ccd)))
+Base.convert(::Type{CCDData{T}}, ccd::CCDData{V}) where {T,V} = CCDData{T}(data(ccd), hdr(ccd))
+Base.view(ccd::CCDData, inds...) = CCDData(view(data(ccd), inds...), hdr(ccd))
+Base.selectdim(ccd::CCDData, d::Integer, idxs) = CCDData(selectdim(data(ccd), d, idxs), hdr(ccd))
 # broadcast mechanics
 Base.BroadcastStyle(::Type{<:CCDData}) = Broadcast.ArrayStyle{CCDData}()
 function Base.similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{CCDData}}, ::Type{T}) where T
     ccd = find_ccd(bc)
-    CCDData(similar(ccd.data, T, axes(bc)), ccd.hdr)
+    CCDData(similar(data(ccd), T, axes(bc)), hdr(ccd))
 end
 "`A = find_ccd(As)` returns the first CCDData among the arguments."
 find_ccd(bc::Base.Broadcast.Broadcasted) = find_ccd(bc.args)
