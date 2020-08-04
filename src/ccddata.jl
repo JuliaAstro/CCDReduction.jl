d60e235940b7c1250dec90ddd6b22c621ce671ef
siddharthlal25 <siddharthlal25@gmail.com>
2020-08-04 07:36:02 +0530

    modified docs

--- src/ccddata.jl
+++ src/ccddata.jl
@@ -14,9 +14,34 @@ Base.promote_rule(::Type{AbstractCCDData{T}}, ::Type{AbstractCCDData{V}}) where 
 
 # custom data type to hold ImageHDU
 """
-    CCDData(data::AbstractMatrix, hdr::FITSHeader)
+    CCDData(data::AbstractMatrix, [hdr::FITSHeader])
 
 Struct to store `ImageHDU`, derived from [`AbstractCCDData`](@ref).
+
+`CCDData` acts like a matrix with a header associated.
+
+```julia
+ccd = CCDData(zeros(4, 4))
+
+ccd[1]
+```
+This accesses the 1st element in matrix associated with `ccd`.
+
+One can perform arithmetic operations on it as well:
+
+```julia
+ccd1 = CCDData(zeros(4, 4))
+
+ccd2 = CCDData(ones(4, 4))
+
+sum_ccd1 = ccd1 + ccd2
+
+sum_ccd2 = ccd2 + ccd1
+```
+
+`sum_ccd1` has the header of `ccd1` whereas `sum_ccd2` has the header `ccd2`.
+
+If header is not provided in the `CCDData` constructor, `default_header` is used to generate the header.
 """
 struct CCDData{T,M<:AbstractMatrix{T}} <: AbstractCCDData{T}
     data::M
@@ -30,11 +55,6 @@ Loads `ImageHDU` as `CCDData`.
 """
 CCDData(hdu::ImageHDU) = CCDData(getdata(hdu), read_header(hdu))
 
-"""
-    CCDData(data::AbstractArray)
-
-Creates `CCDData` from an `Array`.
-"""
 CCDData(data) = CCDData(data, get_default_header(data))
 
 """
