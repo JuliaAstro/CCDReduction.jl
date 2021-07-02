d7a47d79c0c581f99778270b6cf945824826d082
Miles Lucas <mdlucas@hawaii.edu>
2021-07-02 12:08:46 -1000

    update tests to use temp directories to avoid potential failures do to file structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,15 @@ using FITSIO
 
 include("data.jl")
 
-@testset "basic methods" begin
-    include("methods.jl")
+function test_header(ccd1::CCDData, ccd2::CCDData)
+    header1 = ccd1.hdr
+    header2 = ccd2.hdr
+    @test keys(header1) == keys(header2)
+    for (k1, k2) in zip(keys(header1), keys(header2))
+        @test header1[k1] == header2[k2]
+    end
 end
 
-@testset "FITS interface" begin
-    include("fits.jl")
-end
-
-@testset "collection" begin
-    include("collection.jl")
-end
+@testset "basic methods" begin include("methods.jl") end
+@testset "FITS interface" begin include("fits.jl") end
+@testset "collection" begin include("collection.jl") end
