ee47fd1234126a2651e17b774754ce78ce19e427
icweaver <weaveric@gmail.com>
2025-06-04 20:19:35 -0700

    updated links

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,10 @@ DocMeta.setdocmeta!(CCDReduction, :DocTestSetup, :(using CCDReduction); recursiv
 include("pages.jl")
 makedocs(modules = [CCDReduction],
     sitename = "CCDReduction.jl",
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://JuliaAstro.org/CCDReduction/stable/",
+    ),
     authors = "Siddharth Lal",
     pages = pages,
 )
@@ -13,5 +16,6 @@ makedocs(modules = [CCDReduction],
 deploydocs(;
     repo = "github.com/JuliaAstro/CCDReduction.jl.git",
     push_preview = true,
-    devbranch = "main"
+    devbranch = "main",
+    versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
 )
